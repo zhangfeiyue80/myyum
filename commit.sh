f837fd4f17c57d9a59d6af157075e54cdd6918b1
zhangfeiyue80 <2247058872@qq.com>
2018-07-26 18:07:31 +0800

    Thu Jul 26 18:07:30     2018 CSAPP study

---
+++ commit.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+date_time=$(date)
+
+git add ./
+git commit -m "${date_time} CSAPP study"
---
+++ pull.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+git remote add origin https://github.com/zhangfeiyue80/myyum.git
+git pull origin master
